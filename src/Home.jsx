5ed5e7d968c836f065e24accbf9e31a53ac501b3
vicentemarciot27 <vicentemarciot27@gmail.com>
2023-10-02 19:22:35 -0300

    feat: results numbers change

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from './components/Navbar/Navbar'
 import Servicos from './Pages/Servicos/Servicos'
-import Diferenciais from './Pages/Diferenciais/Diferenciais'
 import Carrousel from './components/Hero/Carrousel'
 import Resultados from './Pages/Resultados/Resultados'
 import Contato from './Pages/Contato/Contato'
@@ -12,7 +11,6 @@ const Home = () => {
             <Navbar></Navbar>
             <Carrousel></Carrousel>
             <Servicos></Servicos>
-            <Diferenciais></Diferenciais>
             <Resultados></Resultados>
             <Contato></Contato>
         </>
--- src/Pages/Resultados/Resultados.jsx
+++ src/Pages/Resultados/Resultados.jsx
@@ -1,53 +1,60 @@
 import React from "react";
 import "./Resultados.css";
 import CarouselLogos from "./components/CarouselLogos";
-import Numeros from "./../../components/Numeros/Numeros"
+import Numeros from "./../../components/Numeros/Numeros";
 import SocialProof from "./components/SocialProof/SocialProof";
 
 import { Link } from "react-router-dom";
 
 const Resultados = () => {
-    return (
-        <section id="resultados">
-            <div className="container">
-                <div className="titulo">
-                    <h2>
-                        NOSSOS RESULTADOS
-                        <br />
-                        <span>FALAM POR SI</span>
-                    </h2>
-                    <p className="">
-                        A ITAJúnior tem orgulho de contar seus cases de sucesso
-                        e o resultado de seu trabalho. Nos inspiramos por fazer
-                        a diferença no negócio de nossos clientes, e por isso
-                        somos movidos por desafios.
-                    </p>
-                </div>
+  return (
+    <section id="resultados">
+      <br />
 
-            <SocialProof></SocialProof>
-
+      <div className="container">
+        <div className="titulo">
+          <h2>
+            NOSSOS RESULTADOS
+            <br />
+            <span>FALAM POR SI</span>
+          </h2>
+          <div className="subtitulo-e-numeros">
+            <h3 className="subtitulo-metade">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Laudantium accusantium ex explicabo qui nostrum! Fugit nulla
+              expedita perferendis minima consequuntur dignissimos labore
+              distinctio libero quisquam modi in perspiciatis quos eveniet
+              sapiente quia excepturi ducimus eius dolore sit corporis, quod
+              neque ea. Iure, neque sequi nemo, dignissimos tempora suscipit
+              illum pariatur a in eveniet, officiis voluptate quos fugiat porro
+              itaque sapiente inventore architecto et eum aspernatur deserunt
+              soluta necessitatibus quasi!
+            </h3>
             <Numeros></Numeros>
+          </div>
+        </div>
 
+        <div className="espacado-3rem">
+          <SocialProof></SocialProof>
+        </div>
 
-                
-                <div className="subtitulo">
-                    <h2>
-                        <span>Seja como nossos clientes:</span>
-                    </h2>
-                </div>
-                <div className="clientes">
-                    <br />
-                    <CarouselLogos></CarouselLogos>
-                </div>
-                <br />
-                <Link to="cases" className="btn-mais casesbtn">
-                    Cases de Sucesso
-                </Link>
-            </div>
+        <div className="subtitulo">
+          <h2>
+            <span>Seja como nossos clientes:</span>
+          </h2>
+        </div>
 
-
-        </section>
-    );
+        <div className="clientes">
+          <br />
+          <CarouselLogos></CarouselLogos>
+        </div>
+        <br />
+        <a href="#contato" className="btn-mais casesbtn">
+          Entre em contato!
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Resultados;
