26a1cf6fd39fe50d76353c9f7d7cffd616deb839
Jonatan de Lima <jonatanlima852@gmail.com>
2023-11-21 01:57:00 -0300

    fix: Responsiveness in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer id="footer">
-            <p>
+            <div>
                 Copyright &copy; 2022 ITA Júnior - Todos os direitos reservados
                 - Feito com orgulho por ITA Júnior
-            </p>
+            </div>
         </footer>
     );
 };
