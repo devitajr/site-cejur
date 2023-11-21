7f5700dc83a6dc439f0006f67363e91ebc737e82
Jonatan de Lima <jonatanlima852@gmail.com>
2023-11-21 08:49:50 -0300

    update: create function to renderize events

--- src/Pages/Eventos/Eventos.jsx
+++ src/Pages/Eventos/Eventos.jsx
@@ -5,6 +5,100 @@ import Newsletter from "../../popups/Newsletter/Newsletter";
 
 const Eventos = () => {
 
+    const listaDeEventos = [
+        {
+            nome: "Capacitação do pessoal do CEJUR",
+            descricao: "bla bla bla bla bla blabla bla blabla bla blabla " + 
+            "bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla ",
+            fotos: [
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+            ]
+        },
+        {
+            nome: "Capacitação do pessoal do CEJUR",
+            descricao: "bla bla bla bla bla blabla bla blabla bla blabla " + 
+            "bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla ",
+            fotos: [
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+            ]
+        },
+        {
+            nome: "Capacitação do pessoal do CEJUR",
+            descricao: "bla bla bla bla bla blabla bla blabla bla blabla " + 
+            "bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla ",
+            fotos: [
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+                'https://images.pexels.com/photos/1520483/pexels-photo',
+            ]
+        }
+    ]
+
+    const renderizarEventos = (arrayDeEventos) => {
+        const renderizar = arrayDeEventos.map((evento, id) => {
+            if(id % 2 == 0){
+                return (
+                    <div id="evento-tipo1">
+                        <div className="evento-descricao">
+                            <div className="evento-bloco">
+                                <div className="evento-svg"></div>
+                                <div className="evento-nome">
+                                    Elaboração e Revisão contratual
+                                </div>
+                            </div>
+                            <div className="evento-texto">
+                                <p>{evento.nome}</p>
+                                {evento.descricao}
+                                <div className="evento-palavras-chave">
+                                    Prestação de Serviços - Parceria -
+                                    Vesting - Licença e Cessão de Uso de Marca -
+                                    Confidencialidade - Compra e Venda -
+                                    Termos de Consentimento - Locação - Outros
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            } else {
+                return (
+                    <div id="evento-tipo2">
+                        <div className="evento-descricao">
+                            <div className="evento-texto">
+                                <p>{evento.nome}</p>
+                                {evento.descricao}
+                                <div className="evento-palavras-chave">
+                                    Contrato Social - Estatuto Social -
+                                    Código de Conduta - Licença e Cessão de Uso de Marca -
+                                    Regimento Interno - Termo de Voluntariado -
+                                    Manual de Abertura de CNPJ - Outros
+                                </div>
+                            </div>
+                            <div className="evento-bloco">
+                                <div className="evento-svg"></div>
+                                <div className="evento-nome">
+                                    Documentos
+                                    Constitutivos
+                                    e Auxiliares
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            
+        })
+
+        return renderizar;
+    }
+
+
+
+
+
     return (
         <>
             <section id="eventos">
@@ -30,79 +124,7 @@ const Eventos = () => {
                             <i className="fas fa-solid fa-chevron-down fa-2x"></i>
                         </a>
                     </div>
-                    <div id="evento-tipo1">
-                        <div className="servico-descricao">
-                            <div className="servico-bloco">
-                                <div className="servico-svg"></div>
-                                <div className="servico-nome">
-                                    Elaboração e Revisão contratual
-                                </div>
-                            </div>
-                            <div className="servico-texto">
-                                <p>Elaboração e Revisão</p>
-                                Essencial para qualquer
-                                tipo de pessoa jurídica. Isso
-                                se dá pela importância do
-                                estabelecimento de um
-                                vínculo formal, seja no
-                                formato de uma parceria,
-                                uma prestação de serviços
-                                ou qualquer outra relação
-                                que gere direitos e deveres
-                                das partes. Dessa forma, a
-                                função do contrato é a
-                                determinação dos termos
-                                dessas relações e a
-                                garantia do cumprimento
-                                das obrigações acordadas.
-
-                                <div className="servico-palavras-chave">
-                                    Prestação de Serviços - Parceria -
-                                    Vesting - Licença e Cessão de Uso de Marca -
-                                    Confidencialidade - Compra e Venda -
-                                    Termos de Consentimento - Locação - Outros
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="evento-tipo2">
-                        <div className="servico-descricao">
-                            <div className="servico-texto">
-                                <p>Documentos Constitutivos</p>
-                                Necessários para garantir
-                                às organizações o devido
-                                reconhecimento legal.
-                                Sendo assim, eles contêm
-                                todas as informações
-                                básicas para a existência
-                                formal das instituições e
-                                disciplinam como se dará o
-                                seu funcionamento interno,
-                                seja a participação
-                                atribuída a cada
-                                sócio/membro, a forma de
-                                admissão de outros, e as
-                                responsabilidades dos
-                                cargos de gerência.
-
-
-                                <div className="servico-palavras-chave">
-                                    Contrato Social - Estatuto Social -
-                                    Código de Conduta - Licença e Cessão de Uso de Marca -
-                                    Regimento Interno - Termo de Voluntariado -
-                                    Manual de Abertura de CNPJ - Outros
-                                </div>
-                            </div>
-                            <div className="servico-bloco">
-                                <div className="servico-svg"></div>
-                                <div className="servico-nome">
-                                    Documentos
-                                    Constitutivos
-                                    e Auxiliares
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {renderizarEventos(listaDeEventos)}
                 </div>
             </section>
         </>
