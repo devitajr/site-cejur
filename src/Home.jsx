5126eb1be23d5065b8540881b1ecf5a7fe997e7f
Jonatan de Lima <jonatanlima852@gmail.com>
2023-11-21 01:39:50 -0300

    delete js code from Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from './Pages/Navbar/Navbar'
 import Servicos from './Pages/Servicos/Servicos'
 import MVV from './Pages/MVV/MVV'
-import Carrousel from './components/Hero/Carrousel'
+import Carrousel from './components/Inicio/Carrousel'
 import Resultados from './Pages/Resultados/Resultados'
 import Contato from './Pages/Contato/Contato'
 import Newsletter from './popups/Newsletter/Newsletter'
@@ -13,35 +13,6 @@ import { useState, useEffect } from "react";
 
 
 const Home = () => {
-
-    // const NEWSLETTER_TIMER = 3000;
-
-    // //Code to show newsletter after 15 seconds of the first interaction with the page
-    // const [showNewsletter, setShowNewsletter] = useState(false);
-    // const [hasInteracted, setHasInteracted] = useState(false);
-
-    // useEffect(() => {
-    //     const handleInteraction = () => {
-    //         if (!hasInteracted) {
-    //             setHasInteracted(true);
-    //             setTimeout(() => {
-    //                 setShowNewsletter(true);
-    //             }, NEWSLETTER_TIMER);
-    //         }
-    //     };
-
-    //     window.addEventListener('click', handleInteraction);
-    //     window.addEventListener('keydown', handleInteraction);
-    //     window.addEventListener('scroll', handleInteraction);
-
-    //     return () => {
-    //         window.removeEventListener('click', handleInteraction);
-    //         window.removeEventListener('keydown', handleInteraction);
-    //         window.removeEventListener('scroll', handleInteraction);
-    //     };
-    // }, [hasInteracted]);
-
-
     return (
         <>
             <Navbar></Navbar>
