f8bd0bc879e2c637753bc6408c236c6a8f8bc440
Jonatan de Lima <jonatanlima852@gmail.com>
2023-11-29 01:34:51 -0300

    update: add newsletters

--- src/Pages/Conteudo/Newsletters/Newsletters.jsx
+++ src/Pages/Conteudo/Newsletters/Newsletters.jsx
@@ -6,54 +6,101 @@ import Whatsapp from "../../../popups/BotaoWhatsapp/Whatsapp"
 import './Newsletters.css'
 const Conteudo = () => {
 
-    const pdfDataArray = [
-        {
-          title: 'PDF 1',
-          pdfUrl: '/pdfs/news1.pdf',
-          imageUrl: '/images/pdf1.jpg',
-        },
-        {
-          title: 'PDF 2',
-          pdfUrl: '/pdfs/news1.pdf',
-          imageUrl: '/images/pdf2.jpg',
-        },
-        // Adicione mais objetos para mais PDFs
-      ];
+  const pdfDataArray = [
+    {
+      title: 'Direito em Pauta: Alterações na LGPD',
+      subtitle: 'Uma análise sobre as alterações nas lei de proteção dos dados',
+      data: '09/2023',
+      pdfUrl: '/pdfs/NewsLetter0923.pdf',
+      imageUrl: '/IMG/Newsletters/NewsSetembro.png',
+    },
+    {
+      title: 'Direito em Pauta: Tributário',
+      subtitle: 'Uma análise sobre o direito tributário no Brasil e suas implicações',
+      data: '10/2023',
+      pdfUrl: '/pdfs/NewsLetter1023.pdf',
+      imageUrl: '/IMG/Newsletters/NewsOutubro.png',
+    },
+    {
+      title: 'Direito em Pauta: Alterações na LGPD',
+      subtitle: 'Uma análise sobre as alterações nas lei de proteção dos dados',
+      data: '09/2023',
+      pdfUrl: '/pdfs/NewsLetter0923.pdf',
+      imageUrl: '/IMG/Newsletters/NewsSetembro.png',
+    },
+    {
+      title: 'Direito em Pauta: Tributário',
+      subtitle: 'Uma análise sobre o direito tributário no Brasil e suas implicações',
+      data: '10/2023',
+      pdfUrl: '/pdfs/NewsLetter1023.pdf',
+      imageUrl: '/IMG/Newsletters/NewsOutubro.png',
+    },
+    // Adicione mais objetos para mais PDFs
+  ];
 
 
-      function getNewsletters(pdfData) {
-        const pdfList = [];
-      
-        const pdfHTML = pdfData.map((item, index) => {
-          const { title, pdfUrl, imageUrl } = item;
-          const downloadUrl = `${pdfUrl}`; // Rota de download (substitua por sua lógica)
+  function getNewsletters(pdfData) {
+    const pdfList = [];
 
-          const pdfItem = (
-            <div class="pdf-item">
-              <img src={imageUrl} alt={title} />
-              <h2>${title}</h2>
-              <a href={downloadUrl} download>Download</a>
-            </div> );
+    const pdfHTML = pdfData.map((item, id) => {
+      const { title, data, subtitle, pdfUrl, imageUrl } = item;
+      const downloadUrl = `${pdfUrl}`; // Rota de download 
 
-            return pdfItem
-        });
-      
-        return pdfHTML;
-      }
-
-
-    return (
-        <div id="newsletters-page">
-            <Navbar></Navbar>
-            <Newsletter></Newsletter>
-            <Whatsapp></Whatsapp>
-            <div className="titulo">Newsletters passadas</div>
-            <div className="newsletters-box">
-                {getNewsletters(pdfDataArray)}
+      const pdfItem = (
+        <div key={id} id={"evento" + id} className={id % 2 === 0 ? "news-tipo1" : "news-tipo2"}>
+          <div className="news-img">
+            <img src={imageUrl} alt={title} />
+          </div>
+          <div className="news-texto">
+            <h2>{title} - {data}</h2>
+            <h4>{subtitle}</h4>
+            <div className="botaoDownload">
+              <a href={downloadUrl} download>
+                <i className="fa fa-download fa-2x"></i>
+              </a>
             </div>
+          </div>
         </div>
-        
-    )
+      );
+
+      return pdfItem
+    });
+
+    return pdfHTML;
+  }
+
+  const renderizarEventos = (arrayDeEventos) => {
+    return arrayDeEventos.map((evento, id) => (
+      <div key={id} id={"evento" + id} className={id % 2 === 0 ? "evento-tipo1" : "evento-tipo2"}>
+        <div className="evento-descricao">
+          <div className="evento-bloco">
+            <div className="evento-svg">
+              <CarouselImages id={`carouselExampleIndicators${id}`} fotos={evento.fotos} />
+            </div>
+          </div>
+          <div className="evento-texto">
+            <p>{evento.nome}</p>
+            {evento.descricao}
+          </div>
+        </div>
+
+      </div>
+    ));
+  };
+
+
+  return (
+    <div id="newsletters-page">
+      <Navbar></Navbar>
+      <Newsletter></Newsletter>
+      <Whatsapp></Whatsapp>
+      <div className="titulo">Newsletters passadas</div>
+      <div className="newsletters-box">
+        {getNewsletters(pdfDataArray)}
+      </div>
+    </div>
+
+  )
 }
 
 
