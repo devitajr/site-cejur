a28d4ebb58958ffe8eb089b838eea4d2532a3e16
Jonatan de Lima <jonatanlima852@gmail.com>
2024-03-06 19:16:35 -0300

    fix text

--- src/Pages/Sobre/Historia/Historia.jsx
+++ src/Pages/Sobre/Historia/Historia.jsx
@@ -19,7 +19,7 @@ const Historia = () => {
 
                     <p> O Centro de Estudos Jurídicos Júnior também chegou a atuar na prestação de serviços pro bono para a comunidade mais vulnerável que habitava próximo à Rua Rocha, local em que se situava o prédio da Escola de Direito De São Paulo (EDESP) . </p>
 
-                    <p> Atualmente somos uma empresa júnior jurídica que trabalha com soluções jurídicas, que são consultorias que visam contribuir para a adequação jurídica do cliente diante do mercado legal. Nossos atuais perfis de clientes são ONGs, startups e outras entidades estudantis bem como Projetos Pro- Bono para determinadas organizações </p>
+                    <p> Atualmente somos uma empresa júnior jurídica que trabalha com soluções jurídicas, que são consultorias que visam contribuir para a adequação jurídica do cliente diante do mercado legal. Nossos atuais perfis de clientes são ONGs, startups e outras entidades estudantis bem como Projetos Pro-Bono para determinadas organizações </p>
 
                     <p> A virada de chave no escopo de atuação do CEJUR se deu no ano de 2021, através do ingresso da entidade no MEJ (Movimento Empresa Júnior), que, ainda nos dias de hoje, nos proporciona uma integração essencial com o mercado sênior e outras empresas juniores, fortalecendo, deste modo, a cultura e o propósito de uma entidade que busca cada vez mais a oferta de experiências e aprendizados aos seus membros bem como o constante contato com núcleos expoentes dentro do mundo jurídico o CEJUR.</p>
                 </div>
