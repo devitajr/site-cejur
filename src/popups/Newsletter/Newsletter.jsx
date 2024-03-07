ecd83551801cd7498d83c77def4b2502430e7a2c
Jonatan de Lima <jonatanlima852@gmail.com>
2024-03-07 11:21:58 -0300

    take off white space on navbar

--- src/popups/Newsletter/Newsletter.jsx
+++ src/popups/Newsletter/Newsletter.jsx
@@ -8,7 +8,6 @@ const Newsletter = () => {
   const urlForms = "https://forms.gle/uJEgdckK5nQK6mxA7"
   return (
     <div id="newsletter">
-
       <button type="button" className="botaoNewsletter" data-bs-toggle="modal" data-bs-target="#exampleModal" translate="no" lang="pt">
         <a href={urlForms} target="_blank">
             Acesse nossas Newsletters!
